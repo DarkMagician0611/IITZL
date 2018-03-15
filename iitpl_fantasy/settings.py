cdf735840cb5de2e2fce7ddc5ceb305a610e2e50
Shubham Chaudhary <shubham.bspdesign@gmail.com>
2018-03-15 14:43:28 +0530

    Database Reset

--- iitpl_fantasy/settings.py
+++ iitpl_fantasy/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'iitpl_fantasy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'IITZL',
-        'USER': 'IITZLuser',
-        'PASSWORD': 'xlr8rocks',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
